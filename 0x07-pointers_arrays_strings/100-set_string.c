8c34a05bc997f7b74623e62f5c00f3e0609f6828
Maxiesselowitz <richardesselmensah@gmail.com>
2024-02-29 07:53:24 -0800

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - the entry point
+ *
+ * @s: the input
+ *
+ * @to: the input
+ *
+ * Return: Always 0 (Success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
